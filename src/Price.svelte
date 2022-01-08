2ae005a6937b85c397dd322a1778f01aed552038
Chidam <86545749+chidam333@users.noreply.github.com>
2022-01-08 11:03:27 +0530

    Update Price.svelte

--- src/Price.svelte
+++ src/Price.svelte
@@ -1,66 +1,134 @@
-
-{indexs}
-<div class="grid-video-list">
-    {#each videosIndex as index}
-    <a href="{videosUrl[index]}"class="card card-list" style="width: 18rem;">
-            <h1>{videosTitle[index]}</h1>
-            <h5>{videosDesc[index]}</h5>
-    </a>
-    {/each}
-
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-        <form on:submit={addVideo}>
-            <input class="video-title text" placeholder="Video title" bind:value={vtitle} required/>
-            <textarea class="video-desc" placeholder="Give a brief description about the video. It is highly enouraged" bind:value={vdesc} required></textarea>
-            <input type="text" class="🔗" spellcheck="false" placeholder="paste the url" bind:value={driveurl} required/>
-            <button class="video-btn btn-lg btn-dark" type="submit">Post video</button>
-        </form>
-        </div>
+<script></script>
+<div class="imgtop">
+    <img src="img/contact.jpg" class="img_real" alt="contact_image">
+    <div class="img-overlay">
+      <form action="https://formspree.io/f/xqknknek" method="post">
+        <div class="container">
+          <div class="row">
+              <div class="content">
+                <h3> Get a Quote <img src="https://img.icons8.com/flat-round/48/000000/quote.png" alt="quote_icon"/></h3>
+                  <div class="input">
+                    <div class="input-group">
+                      <input type="name" class="form-control btn0" name="name" placeholder="Your name" >
+                      <input type="email" class="form-control btn1" name="_replyto" placeholder="Your email">
+                    <div class="input-group">
+                      <textarea id="btn2" name="msg" rows="2" cols="85" class="form-control" placeholder="Suggest your ideal car with modifications 🚀"></textarea>
+                      <span class="input-btn">
+                        <button class="btn text-center" type="submit" value="submit">SUBMIT</button>
+                      </span>
+                    </div>
+                  </div>
+                <br>
+                <hr style="color: white;">
+              </div>
+            </div>
+            <h4>Hate doing mails and doing quotation ?</h4><br>
+            <p>Social Media Handles,</p>
+            <p><a href="https://twitter.com/Chidam333" target="_blank" class="link-primary"><img src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt="twitter icon"/></a></p>
+            <p><b>ID: </b><a href="https://twitter.com/Chidam333" target="_blank" class="link-primary">@chidam333</a></p>
+            </div>    
+          </div>
+      </form>
     </div>
 </div>
 
+
 <style>
-    .video-title{
-        color: rgb(0, 0, 0);
-        font-family: "roboto";
-        display: inline-block;
-        margin-bottom: 1rem;
-        width:8rem;
-        text-transform: uppercase;
+    .imgtop{
+        position: relative;
     }
-    .video-desc{
-        color: rgb(77, 65, 65);
-        font-family: "codercoder";
-        display: inline-block;
-        margin-bottom: 1rem;
-        font-size: 0.8rem;
-        min-height: 4rem;
-        width:15rem;
-        text-transform: uppercase;
-        
+    .img_real{
+        display: block;
+        width: 100%;
+        height: 900px;
+        object-fit: cover;
+        object-position: bottom;
     }
-    .🔗{
-        color: steelblue;
-        font-family: "codercoder";
-        letter-spacing: 0.1rem;
-        display: inline-block;
-        margin-bottom: 1rem;
-        font-size: 0.7rem;
-        width:15rem;
-        text-transform: uppercase;
-    }
-    .grid-video-list{
+    .img-overlay{
+        position: absolute;
+        top: 0%;
+        left: 0%;
+        width: 100%;
+        height: 100%;
+        background: rgba(20, 34, 36, 0.8);
         display: grid;
-        gap: 1rem;
-        grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+        justify-content: center;
+        align-content: center;
+        grid-auto-flow: row;
+        margin:0 auto;
+        text-align: center;
     }
-    .card-list{
-        text-decoration: none;
-        padding-left: 5%;
-        background: rgb(36, 36, 36) !important;
-    }
-    .card-list h1{
-        color: wheat;
-    }    
+.row{
+    padding-left:5%;
+    padding-right: 5%;
+    padding-top: 20%;
+    padding-bottom: 20%;
+}
+.row .content{
+    max-width: 650px;
+    margin:0 auto;
+    text-align: center;
+}
+.row .content h3{
+    margin-bottom:40px;
+    color: rgb(240, 214, 166);
+}
+.row .btn0{
+    height: 50px;
+    border-color: #68035b ;
+    border: 3px solid #0ca3c9;
+    font-family: "roboto" ;
+    color: #67990a;
+}
+.row .btn1{
+    height: 50px;
+    border-color: #68035b ;
+    border: 3px solid #0ca3c9;
+    font-family: "roboto" ;
+    color: #67990a;
+}
+.row .btn0:focus{
+    border: 3px solid rgb(223, 103, 103);
+    color:black;
+}
+.row .btn1:focus{
+    border: 3px solid rgb(223, 103, 103);
+    color:black
+}
+.row #btn2{
+    height: 50px;
+    border-color: #68035b ;
+    border: 3px solid #0ca3c9;
+    font-family: "roboto" ;
+    color: #67990a;
+}
+.row #btn2:focus{
+    border: 3px solid rgb(223, 103, 103);
+    color:black;
+}
+.row .content .btn{
+    height: 50px;
+    border-radius: 0;
+    background-color: rgb(0, 100, 75) ;
+    padding-right: 40px;
+    padding-left: 40px;
+    font-family: "roboto";
+    color: wheat;
+}
+.row .content .btn:focus{
+
+    background-color: rgb(153, 106, 4);
+    border: 3px solid black;
+}
+.row h4{
+  color: rgb(240, 214, 166);
+  font-family: "ele";
+  word-spacing: 0.6rem;
+  font-weight: 900;
+}
+.row p{
+  color: rgb(198, 248, 248);
+  font-family: "robo";
+  font-size: 25px;
+}
 </style>
